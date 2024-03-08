4f1ec69d3edc0fcad382922e646cdee5b445aa65
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-08 14:25:15 -0600

    Added the delete promise and listener

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -27,6 +27,7 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
+// Created the promise to post all the notes
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
 
@@ -35,10 +36,31 @@ app.post('/api/notes', (req, res) => {
         const note = JSON.parse(data);
         newNote.id = Date.now(); 
         note.push(newNote);
+        // Used append instead of write to store multiple notes
+        fs.appendFile(dbNotes, JSON.stringify(note), (err) => {
+            if (err) throw err;
+            res.send('New note posted successfully');
+        });
+    });
+});
+
+// Created the promise to delete the notes
+app.delete('/api/notes', (req, res) => {
+    const idNote = parseInt(req.params.id);
+
+    fs.readFile(dbNotes, 'utf8', (err, data) => {
+        if(err) throw err;
+        let note = JSON.parse(data);
+        note = note.filter(note => note.id !== idNote);
 
         fs.appendFile(dbNotes, JSON.stringify(note), (err) => {
             if (err) throw err;
-            res.send('New note stored successfully');
+            res.send('New note deleted successfully');
         });
     });
+});
+
+// Listener for the whole file and creating the server link
+app.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}`);
 });
