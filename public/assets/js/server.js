d12ce482c4cf1e49d40fca985f34fe9b2507b90b
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-08 13:25:13 -0600

    Declared the initial consts

---
+++ public/assets/js/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+
