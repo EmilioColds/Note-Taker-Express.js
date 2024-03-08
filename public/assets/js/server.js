5837ec2f578627c43397e18a90a8d61c679f0994
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-08 13:30:11 -0600

    Added the middleware and the notes route

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,3 +1,4 @@
+// Declared the constants with require
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,4 +7,12 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+// Declared the middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
+//Created the route for the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
